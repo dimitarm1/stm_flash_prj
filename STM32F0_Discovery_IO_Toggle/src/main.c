76afd773cef417eb5b198b0d07af5ea6b47d3f7f
dmarinov <dimitar.m.marinov@abv.bg>
2015-08-21 13:02:28 +0300

    Pulse generated at PA8 MCO
    Signed-off-by: dmarinov <dimitar.m.marinov@abv.bg>

--- STM32F0_Discovery_IO_Toggle/src/main.c
+++ STM32F0_Discovery_IO_Toggle/src/main.c
@@ -382,6 +382,81 @@ void show_digit(int digit){
  * DP
  */
 
+
+/**
+* Sets System clock frequency to 24MHz and configure HCLK, PCLK2 and PCLK1 prescalers.
+*
+* param None
+* retval None
+*/
+void SetSysClockTo24(void)
+{
+	ErrorStatus HSEStartUpStatus;
+	/* SYSCLK, HCLK, PCLK2 and PCLK1 configuration -----------------------*/
+	/* RCC system reset(for debug purpose) */
+	RCC_DeInit();
+	/* Enable HSE */
+	RCC_HSEConfig(RCC_HSE_ON);
+	/* Wait till HSE is ready */
+	HSEStartUpStatus = RCC_WaitForHSEStartUp();
+	if (HSEStartUpStatus == SUCCESS)
+	{
+		/* Flash 0 wait state */
+		FLASH_SetLatency(FLASH_Latency_0);
+		/* HCLK = SYSCLK */
+		RCC_HCLKConfig(RCC_SYSCLK_Div1);
+		/* PCLK2 = HCLK */
+		RCC_PCLK2Config(RCC_HCLK_Div1);
+		/* PCLK1 = HCLK */
+		RCC_PCLK1Config(RCC_HCLK_Div1);
+		/* PLLCLK = (8MHz/2) * 6 = 24 MHz */
+	//	RCC_PREDIV1Config(RCC_PREDIV1_Source_HSE, RCC_PREDIV1_Div2);
+	//	RCC_PLLConfig(RCC_PLLSource_HSE_Div1, RCC_PLLMul_6);
+		RCC_PLLConfig(RCC_PLLSource_HSI_Div2, RCC_PLLMul_14);
+		/* Enable PLL */
+		RCC_PLLCmd(ENABLE);
+		/* Wait till PLL is ready */
+		while (RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET)
+		{
+		}
+		/* Select PLL as system clock source */
+		RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);
+		/* Wait till PLL is used as system clock source */
+		while (RCC_GetSYSCLKSource() != 0x08)
+		{
+		}
+	}
+	else
+	{
+		/* If HSE fails to start-up, the application will have wrong clock configuration.
+	User can add here some code to deal with this error */
+		/* Go to infinite loop */
+		while (1)
+		{
+		}
+	}
+}
+
+/**
+  * @brief  Adjusts the Internal High Speed oscillator (HSI)
+  * @param  HSICalibrationValue: specifies the calibration trimming value.
+  *   This parameter must be a number between 0 and 0x1FF.
+  * @retval None
+  */
+void RCC_AdjustHSI(uint8_t HSICalibrationValue)
+{
+#define CR_HSICAL_Mask           ((uint32_t)0xFFFFF0FF)
+#define CR_HSITRIM_Mask           ((uint32_t)0xFFFFFF07)
+  uint32_t tmpreg = 0;
+  tmpreg = RCC->CR;
+  /* Clear HSITRIM[4:0] bits and HSICAL[7:0] bits */
+  tmpreg &= CR_HSITRIM_Mask & CR_HSICAL_Mask;
+  /* Set the HSITRIM[4:0] and HSICAL[7:0] bits according to HSICalibrationValue value */
+  tmpreg |= (uint32_t)HSICalibrationValue << 3;
+  /* Store the new value */
+  RCC->CR = tmpreg;
+}
+
 /**
  * @brief  Main program.
  * @param  None
@@ -390,7 +465,9 @@ void show_digit(int digit){
 volatile int stop=1;
 int main(void)
 {
+	stm32_Init ();
 //	while (stop);
+	SetSysClockTo24();
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
 	/* Output clock on MCO pin ---------------------------------------------*/
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
@@ -399,6 +476,10 @@ int main(void)
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
 	// RCC_MCOConfig(RCC_MCO_HSE); // Put on MCO pin the: freq. of external crystal
 	RCC_MCOConfig(RCC_MCO_SYSCLK); // Put on MCO pin the: System clock selected
+	//RCC_AdjustHSICalibrationValue(0x00);
+//	RCC_AdjustHSI(0x1ff);
+	RCC_AdjustHSI(0x0);
+
 	while(1);
 
 	read_eeprom();
